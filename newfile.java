49b0ae58b3ccf6cd63233696082e1a433ab1a3cd
MALLIKA2509 <mallikae406@gmail.com>
2025-08-23 17:36:29 +0530

    Add files via upload

---
+++ newfile.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+      //THIS IS MY FIRST JAVA PROGRAM
+
+       /*
+           This
+           is
+           a
+           multi-line
+           comment
+        */
+
+        System.out.println("I like pizza!");
+        System.out.println("It's really pizza!");
+        System.out.println("Buy me pizza!");
+     }
+}
